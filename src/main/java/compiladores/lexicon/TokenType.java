a4e7a7d6c9ec6dfddda44e377b4a57830644defe
Rysen-Winters <lcastinelli@gmail.com>
2026-04-24 11:52:00 -0300

    Created TokenType

---
+++ src/main/java/compiladores/lexicon/TokenType.java
@@ -0,0 +1,69 @@
+package compiladores.lexicon;
+
+// Alfabeto = {a-z,A-Z,0-9,_,.,(,),[,],{,},;,=,+,-,*,/,<,>,!,¡,¿,?,&,|,",\}
+
+public enum TokenType {
+    // Identificadores
+    IDMETAT, // (a-z)(a-z,A-Z,0-9)*
+    IDCLASS, // (A-Z)(a-z,A-Z,0-9)*
+    IDCLASSINT, // Int 
+    IDCLASSSTR, // Str 
+    IDCLASSBOOL, // Bool 
+    IDCLASSARRAY, // Array 
+    IDCLASSIO, // IO 
+    IDCLASSOBJECT, // Object 
+    IDCLASSITERATOR, // Iterator 
+
+    // Literales
+    LITINT, // (0-9)(0-9)*
+    LITSTR, // "(a-z,A-Z,0-9,_,.,(,),[,],{,},;,=,+,-,*,/,<,>,!,¡,¿,?,&,|,",\)*"
+
+    // Simbolos del código
+    PARABRE,  // (
+    PARCIERRA, // )
+    CORABRE, // [
+    CORCIERRA, // ]
+    LLAVEABRE, // {
+    LLAVECIERRA, // }
+    PUNTO, // .
+    PUNTOYCOMA, // ;
+
+    // Palabras reservadas
+    PRCLASS, // class 
+    PRIMPL, // impl 
+    PRIF, // if 
+    PRELSE, // else 
+    PRTRUE, // true 
+    PRFALSE, // false 
+    PRNIL, // nil 
+    PRRET, // ret 
+    PRWHILE, // while 
+    PRFOR, // for 
+    PRNEW, // new 
+    PRFN, // fn 
+    PRST, // st 
+    PRPUB, // pub 
+    PRSELF, // self 
+    PRDIV, // div 
+    PRIN, // in 
+    PRVOID, // void 
+
+    // Operaciones
+    OPSUMA, // +
+    OPRESTA, // - 
+    OPMULT, // * 
+    OPDIVENT, // / 
+    OPINCR, // ++
+    OPDECR, // -- 
+    OPAND, // &&
+    OPOR, // ||
+    OPNOT, // !
+    OPMENOR, // <
+    OPMENORIGUAL, // <=
+    OPIGUAL, // ==
+    OPMAYOR, // >
+    OPMAYORIGUAL, // >=
+    OPDISTINTO, // !=
+    OPASIGN, // =
+
+}
